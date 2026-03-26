164b5e2674c327f5127468f20bed631c3a4768a1
kon khmer <sopheapshop16@gmail.com>
2026-03-26 11:49:09 +0700

    Create brother-script-prism.lua with Brother Script: Prism Edition code

---
+++ brother-script-prism.lua
@@ -0,0 +1,10 @@
+-- Brother Script: Prism Edition
+
+-- This script does the following:
+-- Example code goes here
+
+function main()
+    print("Hello, Brother Script: Prism Edition!")
+end
+
+main()
